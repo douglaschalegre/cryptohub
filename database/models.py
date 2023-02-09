872cdb6ba72b0cf4922e25b5b8b66eeb9081f6b4
Douglas Chalegre <douglas.chalegre@gmail.com>
2023-02-08 22:37:10 -0300

    feat: Creates user and favorite ORM models

---
+++ database/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+base = declarative_base()
+
+class User(base):
+  __tablename__ = 'user'
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  name = Column(String)
+  favorites = relationship('Favorite', backref='user')
+
+class Favorite(base):
+  __tablename__ = 'favorite'
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  symbol = Column(String)
+  user_id = Column(Integer, ForeignKey('user.id'))
+
