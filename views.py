b9cc278b9b94fff21feb7ecc8d7b3a600eebc352
Douglas Chalegre <douglas.chalegre@gmail.com>
2023-02-17 19:41:16 -0300

    feature: Adds description to route documentation

--- views.py
+++ views.py
@@ -6,7 +6,7 @@ from schemas import UserCreateInput, DefaultOutput, AlternativeOutput
 user_router = APIRouter(prefix='/user')
 assets_router = APIRouter(prefix='/assets')
 
-@user_router.post('/create', response_model=DefaultOutput, responses={418: {'model': AlternativeOutput}})
+@user_router.post('/create', description='This route creates a new user', response_model=DefaultOutput, responses={418: {'model': AlternativeOutput}})
 async def user_create(user_input: UserCreateInput):
   try:
     await UserService.create_user(name=user_input.name)
