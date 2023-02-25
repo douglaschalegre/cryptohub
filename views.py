df2ad0c5586eff3dbd1983da68780e735de1fbc0
Douglas Chalegre <douglas.chalegre@gmail.com>
2023-02-25 00:11:56 -0300

    feature: Creates view for adding a favorite to a user

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
 
-from services import UserService
-from schemas import UserCreateInput, DefaultOutput, ErrorOutput
+from services import UserService, FavoriteService
+from schemas import UserCreateInput, DefaultOutput, ErrorOutput, UserFavoriteAddInput
 
 user_router = APIRouter(prefix='/user')
 assets_router = APIRouter(prefix='/assets')
@@ -19,5 +19,13 @@ async def user_create(user_id: int):
   try:
     await UserService.delete_user(user_id)
     return DefaultOutput(message='User deleted')
+  except Exception as error:
+    raise HTTPException(400, detail=str(error))
+
+@user_router.post('/favorite/add', description='This route add a new favorite to a user', response_model=DefaultOutput, responses={400: {'model': ErrorOutput}})
+async def user_favorite_add(favorite_add: UserFavoriteAddInput):
+  try:
+    await FavoriteService.add_favorite(user_id=favorite_add.user_id, symbol=favorite_add.symbol)
+    return DefaultOutput(message='OK')
   except Exception as error:
     raise HTTPException(400, detail=str(error))
