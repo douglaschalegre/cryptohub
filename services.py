20a06e676a380b0083c6745b63737aad028c4805
Douglas Chalegre <douglas.chalegre@gmail.com>
2023-02-26 22:42:53 -0300

    feature: Creates a service for removing favorite symbol

--- services.py
+++ services.py
@@ -18,3 +18,8 @@ class FavoriteService:
     async with async_session() as session:
       session.add(Favorite(user_id=user_id, symbol=symbol))
       await session.commit()
+  
+  async def remove_favorite(user_id: int, symbol: str):
+    async with async_session() as session:
+      session.execute(delete(Favorite).where(Favorite.user_id==user_id, Favorite.symbol==symbol))
+      await session.commit()
