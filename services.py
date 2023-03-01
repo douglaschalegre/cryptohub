eac77f7409e12f1c1b88aa14f6bf9529de1bf174
Douglas Chalegre <douglas.chalegre@gmail.com>
2023-03-01 17:04:10 -0300

    feature: creating AssetService and a get user by id service

--- services.py
+++ services.py
@@ -1,3 +1,5 @@
+from datetime import date, timedelta
+from aiohttp import ClientSession 
 from database.models import User, Favorite
 from database.connection import async_session
 from sqlalchemy import delete
@@ -18,6 +20,11 @@ class UserService:
     async with async_session() as session:
       result = await session.execute(select(User))
       return result.scalars().all()
+  
+  async def get_user_by_id(user_id: int):
+    async with async_session() as session:
+      result = await session.execute(select(User).where(User.id==user_id))
+      return result.scalar()
 
 class FavoriteService:
   async def add_favorite(user_id: int, symbol: str):
@@ -29,3 +36,13 @@ class FavoriteService:
     async with async_session() as session:
       session.execute(delete(Favorite).where(Favorite.user_id==user_id, Favorite.symbol==symbol))
       await session.commit()
+
+class AssetService:
+  async def day_summary(symbol: str):
+    async with ClientSession() as session:
+      yesterday = date.today() - timedelta(days=1)
+      url = f'https://www.mercadobitcoin.net/api/{symbol}/day-summary/{yesterday.year}/{yesterday.month}/{yesterday.day}/'
+      response = await session.get(url)
+      data = await response.json()
+      data['symbol'] = symbol
+      return data
